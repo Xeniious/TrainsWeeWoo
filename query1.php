136e94f886aa3153cf7868ce80396a2d2ffbb173
Chase Burkdoll <chaseburkdoll@ku.edu>
2023-04-30 14:01:33 -0500

    added php and set links in html

---
+++ query1.php
@@ -0,0 +1,29 @@
+<?php
+	$sqlusername = 'c747b428';
+	$sqlpassword = 'ni9Theem';
+	// Connect to MySQL server, select database
+		$conn = mysql_connect('mysql.eecs.ku.edu', $sqlusername, $sqlpassword)
+				or die('Could not connect: ' . mysql_error());
+		//echo 'Connected successfully';
+		mysql_select_db($sqlusername) or die('Could not select database');
+
+	// Send SQL query
+		$query = 'SELECT DISTINCT CONDUCTOR FROM TRIP JOIN TRAIN ON TRAIN.TRAINNAME = TRIP.TRAINNAME';
+		$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+	// Print results in HTML
+		echo "<select>";
+		while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+				foreach ($line as $col_value) {
+					echo "<option value='$col_value'>$col_value</option>";
+				}
+		}
+		echo "</select>";
+		echo "<input type='submit'>";
+
+	// Free resultset
+		mysql_free_result($result);
+
+	//Close connection
+		mysql_close($link);
+?> 
---
+++ tripsquery.php
@@ -0,0 +1,26 @@
+<?php
+	$sqlusername = 'c747b428';
+	$sqlpassword = 'ni9Theem';
+	// Connect to MySQL server, select database
+		$conn = mysql_connect('mysql.eecs.ku.edu', $sqlusername, $sqlpassword)
+				or die('Could not connect: ' . mysql_error());
+		//echo 'Connected successfully';
+		mysql_select_db($sqlusername) or die('Could not select database');
+
+	// Send SQL query
+		$query = 'SELECT * FROM TRIP JOIN STATION ON STATION.CITY = TRIP.DEPARTFROM';
+		$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+	// Print results in HTML
+		while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+				foreach ($line as $col_value) {
+					echo "<p>$col_value</p>";
+				}
+		}
+
+	// Free resultset
+		mysql_free_result($result);
+
+	//Close connection
+		mysql_close($link);
+?> 
